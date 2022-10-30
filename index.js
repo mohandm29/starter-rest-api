e7f0b997b36db29e90c98ee8d2a198b765c24e08
Mohan Kumar <mohan@Mohans-MacBook-Pro.local>
2022-10-30 16:53:39 +0530

    https

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const db = require('cyclic-dynamodb')
 const fetch = require('node-fetch');
+const https = require('https');
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
