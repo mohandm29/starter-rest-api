a956851dec2f5cf6f309ad8d89006e63262ad51b
Mohan Kumar <mohan@Mohans-MacBook-Pro.local>
2022-11-01 22:32:14 +0530

    RG with preopen

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const db = require('cyclic-dynamodb')
 const fetch = require('node-fetch');
 const array = require('lodash/array');
 const object = require('lodash/fp/object');
-
+const marketdata = require('./market-data.json')
+const preOpenJson = require('./pre-open.json')
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
 
 // #############################################################################
 // This configures static hosting for files in /public that have the extensions
@@ -31,7 +33,7 @@ const vinay_chatId = '841550544';
 //group  = -763158766
 const chatId = process.env.chatId;
 const dbData = {'shooting' : [], 'hammer': []};  
-
+const rgdata = {'buy_above_high' :[], 'sell_below_low' :[], 'buy_within_high' :[], 'sell_within_low' :[]}; 
 app.post('/hammer', async (req, res) => {
 
   const scanData = req.body;
@@ -104,39 +106,69 @@ app.get('/cleardata', async (req, res) => {
 app.get('/processrg', async (req, res) => {
 
 
-      const response = await fetch('https://www.nseindia.com',{  method: 'GET',
-        credentials: 'include'
-      });
-    const body = await response.text();
-  // const  headers = await response.headers;
-   console.log(body);
-   // console.log(headers);
-    console.log(response.headers.raw());
-    console.log(response.headers.raw()['set-cookie']);
+  const myHeaders =  {
+    'authority': 'www.nseindia.com',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'accept-language': 'en-US,en;q=0.9',
+    'cache-control': 'no-cache',
+    //'cookie': 'RT="z=1&dm=nseindia.com&si=d8b5b0d2-c9f0-4da8-b4e6-89390de0c451&ss=l9y74brc&sl=4&tt=8z8&bcn=%2F%2F684d0d44.akstat.io%2F&ul=12mox"',
+    'cookie': '',
+    'pragma': 'no-cache',
+    'sec-ch-ua': '"Chromium";v="106", "Google Chrome";v="106", "Not;A=Brand";v="99"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"macOS"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'none',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'
+  }
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders
+  };
 
+  let preopen = {};
+  //TODO 
+  //const data = preOpenJson;
+ const response =  await fetch("https://www.nseindia.com/api/market-data-pre-open?key=FO", requestOptions);
+ if (response.ok) {
+  const data = await response.json();
+  data.data.forEach(element => {
+    preopen[element.metadata.symbol] = element.metadata.lastPrice;
+    console.log(element.metadata.symbol + element.metadata.lastPrice)
+  });
 
-    //console.log(body);
-
-//   const cookiereq = await fetch(nseUrl, { method: 'GET',
-//   credentials: 'include'
-//     });
-// //   const res2 = await fetch(nseUrl+'market-data-pre-open?key=FO',{  method: 'GET',
-// //   credentials: 'include'
-// // });
-//   if (cookiereq.ok) {
-//     const data = await cookiereq.body;
-//     console.log(data);
-//     console.log("response from nse fetch");
-//     return data;
-//   }else{
-//     console.log(" error to get nse data");
-//   }
-  //var preopen = await nseFetch('market-data-pre-open?key=FO');
-  //var lastDay = await nseFetch('equity-stockIndices?index=SECURITIES IN F&O');
-
- // console.log(preopen);
-  //console.log(lastDay);
-  res.end();
+  marketdata.data.forEach(md => {
+    const lasthigh = md.dayHigh;
+    const lastlow = md.dayLow;
+    const lastclose = md.lastPrice;
+    const lastopen = md.open;
+    const latestOpen = preopen[md.symbol];
+    // case 1 previous Red candle 
+    if(lastopen > lastclose) { 
+        if(latestOpen >= lasthigh) {
+          array.concat(rgdata.buy_above_high,md.symbol);
+          console.log('found');
+        } else if(lastclose < lastopen < lasthigh) {
+          array.concat(rgdata.buy_within_high,md.symbol);
+          console.log('found');
+        }
+    } else if (lastopen < lastclose) {
+      if(latestOpen <= lastlow) {
+        array.concat(rgdata.sell_below_low,md.symbol);  
+        console.log('found');
+      } else if(lastlow < lastopen < lastclose) {
+        array.concat(rgdata.sell_within_low,md.symbol);  
+        console.log('found');
+      }
+    }
+  });
+} else {
+  console.log(" error send data"+response.error);
+}
+  res.json(rgdata).end();
 })
 
 async function sendMessage(chat_id,text) {
